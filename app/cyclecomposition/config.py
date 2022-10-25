f27e179906a360a8b3856200e395c836384ee9c3
Erwan JOLIVET <erwan.jolivet.pro@gmail.com>
2022-10-25 16:56:39 +0200

    Fix application port defined in Docker image

--- app/cyclecomposition/config.py
+++ app/cyclecomposition/config.py
@@ -3,5 +3,6 @@ import os
 
 def get_api_url() -> str:
     host = os.environ.get("API_HOST", "localhost")
-    port = 8000 if host == "localhost" else 8000
+    print("HOST: " + host)
+    port = 8000 if host == "localhost" else 5005
     return f"http://{host}:{port}"
